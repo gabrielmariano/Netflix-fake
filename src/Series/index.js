52c64f873772c271f3ca42f22c8f70c8d41e3186
Gabriel Mariano <42684849+gabrielmariano@users.noreply.github.com>
2020-06-10 17:20:56 -0300

    Add files via upload

--- src/Series/index.js
+++ src/Series/index.js
@@ -1,33 +1,75 @@
 import React from "react";
 
-import { AiTwotoneHome } from "react-icons/all";
+import {
+  Wrapper,
+  Header,
+  Nav,
+  Item,
+  Others,
+  Search,
+  Text,
+  Box,
+  Bell,
+  Text1,
+  Text2,
+  Btn,
+  Btn2,
+  
 
-import { Wrapper, Header, Nav, Item, Others, Search, Text, Box, Bell } from "./styles";
+  
 
-import logo from '../../assets/images/logo.png';
-import search from '../../assets/images/search.png';
-import box from '../../assets/images/gift-box.png';
-import notification from '../../assets/images/notification.png';
+} from "./styles";
+
+import logo from "../../assets/images/logo.png";
+import search from "../../assets/images/search.png";
+import box from "../../assets/images/gift-box.png";
+import notification from "../../assets/images/notification.png";
+import walkinglogo from "../../assets/images/walkinglogo.webp";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
 
 function Main() {
   return (
     <Wrapper>
-      <Header><img src={logo}/>x
-          <Nav>
-            <Item>Início</Item>
-            <Item>Séries</Item>
-            <Item>Filmes</Item>
-            <Item>Mais Recentes</Item>
-            <Item>Minha Lista</Item>
-          </Nav>
+      <Header>
+        <img src={logo} />
+        <Nav>
+          <Item>Início</Item>
+          <Item>Séries</Item>
+          <Item>Filmes</Item>
+          <Item>Mais Recentes</Item>
+          <Item>Minha Lista</Item>
+        </Nav>
+        <Others>
+          <Search>
+            <img src={search} />
+          </Search>
+          <Text>
+            <p>INFANTIL</p>
+          </Text>
+          <Box>
+            <img src={box} />
+          </Box>
+          <Bell>
+            <img src={notification} />
+          </Bell>
+        </Others>
+      </Header>
 
-          <Others>
-            <Search><img src={search}/></Search>
-            <Text><p>INFANTIL</p></Text>
-            <Box><img src={box}/></Box>
-            <Bell><img src={notification}/></Bell>
-          </Others>
-    </Header>
+      <img src={walkinglogo} />
+      <Text1>Top 5 séries de hoje</Text1>
+      <Text2>
+        Na série de terror de maior sucesso na TV, quem restou
+        <br />
+        na humanidade precisa lutar com zumbis-e entre si.
+      </Text2>
+
+            <Btn>Assistir</Btn>
+            <Btn2>Mais Informações</Btn2>
+        
+        
+        
+      
     </Wrapper>
   );
 }
--- src/Series/styles.js
+++ src/Series/styles.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import fundo from "../../assets/images/walking.webp";
-import { FaEdge } from "react-icons/fa";
 
 export const Wrapper = styled.div`
   height: 100vh;
@@ -13,16 +12,25 @@ export const Wrapper = styled.div`
   left: 0;
   right: 0;
   top: 0;
+
+  img {
+    margin-left: 4%;
+    margin-top: 3%;
+    width: 34%;
+  }
 `;
 
 export const Header = styled.h1`
-    
-    background-image: linear-gradient(to bottom,rgba(0,0,0,.8) 10%,rgba(0,0,0,0));
-        img {
-        width: 10%;
-        margin-top: -2%;
-        margin-left: 4%;
-    }
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.8) 10%,
+    rgba(0, 0, 0, 0)
+  );
+  img {
+    width: 10%;
+    margin-top: -2%;
+    margin-left: 4%;
+  }
 `;
 
 export const Nav = styled.h1`
@@ -37,9 +45,6 @@ export const Nav = styled.h1`
   -webkit-box-align: center;
   margin-left: 16%;
   margin-top: -3%;
-  
-    
-
 `;
 
 export const Item = styled.h1`
@@ -49,40 +54,79 @@ export const Item = styled.h1`
   padding: 9px;
   font-weight: normal;
   margin-top: -5%;
+  font-size: 1.2rem;
 `;
 
-export const Others = styled.h1`
-`;
-
+export const Others = styled.h1``;
 
 export const Search = styled.div`
-    margin-left: 71%;
-    width: 15%;
-    margin-top: -5.5%;
+  margin-left: 71%;
+  width: 15%;
+  margin-top: -5.5%;
 `;
 
 export const Text = styled.div`
-     margin-left: 75%;
-     margin-top: -2.8%;
-     font-size: 14px;
-     color: #e5e5e5;
-     font-family: Netflix Sans;
-    font-weight: lighter;
-    
+  margin-left: 75%;
+  margin-top: -2.9%;
+  font-size: 14px;
+  color: #e5e5e5;
+  font-family: Netflix Sans;
+  font-weight: lighter;
 `;
 
-
 export const Box = styled.div`
-     margin-left: 81%;
-     margin-top: -3.5%;
-     width: 15%;
+  margin-left: 81%;
+  margin-top: -2.8%;
+  width: -15%;
 `;
 
 export const Bell = styled.div`
-     margin-left: 85%;
-     margin-top: -4.6%;
-     width: 15%;
-     
-    
-    
+  margin-left: 85%;
+  margin-top: -4%;
+  width: -15%;
+`;
+
+export const Text1 = styled.h1`
+  font-family: Netflix Sans;
+  color: white;
+  margin-top: 1.7%;
+  color: #fff;
+  line-height: normal;
+  width: 100%;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  margin-left: 4%;
+  font-size: 1.6vw;
+  font-weight: 700;
+`;
+
+export const Text2 = styled.h1`
+  font-family: Netflix Sans;
+  color: white;
+  margin-top: 2%;
+  color: #fff;
+  font-weight: 400;
+  line-height: normal;
+  width: 100%;
+  font-size: 1.4vw;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  margin-left: 4%;
+`;
+
+export const Btn = styled.button`
+  background-color: white;
+  width: 90px;
+  margin-left: 4%;
+  height: 6%;
+  border-radius: 6px;
+  font-family: Netflix Sans;
+  
+`;
+
+export const Btn2 = styled.button`
+  background-color: white;
+  width: 18%;
+  margin-left: 2%;
+  height: 6%;
+  border-radius: 6px;
+  font-family: Netflix Sans;
 `;
